c3247b3547785690c130a983be9baaaeed7f1758
Viacheslav Dubovskyi <architect1slava@gmail.com>
2025-07-18 12:46:05 +0300

    add fetch sql

---
+++ src/fetch.sql
@@ -0,0 +1,25 @@
+SELECT *
+FROM Homework;
+
+SELECT *
+FROM Lesson
+         LEFT JOIN Homework ON Lesson.homework_id = Homework.id;
+
+SELECT *
+FROM Lesson
+         LEFT JOIN Homework ON Lesson.homework_id = Homework.id
+ORDER BY updatedat;
+
+SELECT s.name      AS schedule_name,
+       s.updatedAt AS schedule_updated,
+       l.name      AS lesson_name,
+       l.updatedAt AS lesson_updated
+FROM schedule s
+         LEFT JOIN lesson l ON s.lesson_id = l.id
+         LEFT JOIN homework h ON l.homework_id = h.id
+         LEFT JOIN schedule_lesson ON schedule_id = schedule_lesson.lesson_id;
+
+SELECT Schedule.name, count(Schedule.lesson_id) AS lesson_count
+FROM Schedule
+         LEFT JOIN schedule_lesson sl on Schedule.id = sl.schedule_id
+GROUP BY Schedule_id, Schedule.name;
